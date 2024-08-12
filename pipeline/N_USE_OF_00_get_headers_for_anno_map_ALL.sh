619df293c41f92c4162347057c69cd5638a55b59
Lotus Lofgren <LLofgren@umn.edu>
2024-08-12 11:56:07 -0400

    add col reporting the length of each protein sequence in the OG N_USE_OF_00_get_headers_for_anno_map_ALL.sh

--- pipeline/N_USE_OF_00_get_headers_for_anno_map_ALL.sh
+++ pipeline/N_USE_OF_00_get_headers_for_anno_map_ALL.sh
@@ -10,10 +10,13 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=common
 
+##this script pulls out the headers from the fastas of each OG, creating one output file for each orthogroup with 4 cols
+##1) the OG#, the sp, the protin ID, and the length of each protein sequence 
+
 #create the output directory if it doesn't exist
 mkdir -p headers
 
-#process each .fa file in the specified directory
+#process each .fa file in the OG directory
 for input_file in /hpc/group/vilgalyslab/lal76/Suillus/N_use/OF_results/Results_May22/Orthogroup_Sequences/*.fa
 do
     #extract the base name of the input file (this will be the OG number)
@@ -23,14 +26,27 @@ do
     output_file="headers/${base_name}_headers.txt"
     
     #process the file
-    awk -v og="$base_name" '
-    /^>/ {
-        sub(/^>/, "");  # Remove the ">" at the start
-        split($0, parts, "|");  # Split the header on "|"
-        split(parts[2], id_parts, "_");  # Split the second part on "_"
-        printf "%s\t%s\t%s\n", og, parts[1], id_parts[2];
+    sed -e 's/\r//g' "$input_file" | awk -v og="$base_name" '
+    BEGIN {
+        RS = ">"
+        FS = "\n"
     }
-    ' "$input_file" > "$output_file"
+    NR > 1 {
+        header = $1
+        split(header, parts, "|")
+        species = parts[1]
+        split(parts[2], id_parts, "_")
+        id = id_parts[2]
+        
+        seq = ""
+        for (i=2; i<=NF; i++) {
+            seq = seq $i
+        }
+        gsub(/[^A-Za-z]/, "", seq)
+        
+        printf "%s\t%s\t%s\t%d\n", og, species, id, length(seq)
+    }
+    ' > "$output_file"
     
     echo "Processed $input_file -> $output_file"
 done
